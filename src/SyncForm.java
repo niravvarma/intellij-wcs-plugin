7fd24ba420b31eca5e8ee66eb18c06b0ff4c4bb7
Rui Lopes da Silva <rui.lopes.silva@celfocus.com>
2016-01-09 00:00:45 +0000

    Change plugin xml information Cleaned the code

--- src/SyncForm.java
+++ src/SyncForm.java
@@ -15,28 +15,18 @@ public class SyncForm extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent event) {
-        //TextConsoleBuidlerFactory.getInstance().createBuilder(project).getConsole()
         Project project = event.getData(PlatformDataKeys.PROJECT);
         JFrame frame = WindowManager.getInstance().getFrame(project);
         final SyncWindowForm syncWindowForm = new SyncWindowForm();
         syncWindowForm.display(frame);
-        //JFrame form= new JFrame("Synchronize Data");
-        //form.setIconImage();
-        //frame.setContentPane(new SyncWindowForm().getMainPanel());
-
-        //frame.setSize(1000,700);
-        //frame.setMinimumSize(new Dimension(1000,700));
-        //frame.setLocationRelativeTo(null);
-        //frame.setVisible(true);
-        // TODO: insert action logic here
     }
 
     @Override
     public void update(AnActionEvent e) {
-        if (PropertiesComponent.getInstance().getBoolean("wcs-plugin-active")){
+        if (PropertiesComponent.getInstance().getBoolean("wcs-plugin-active")) {
             Object navigatable = e.getData(CommonDataKeys.NAVIGATABLE);
             e.getPresentation().setEnabledAndVisible(navigatable != null);
-        }else{
+        } else {
             return;
         }
     }
